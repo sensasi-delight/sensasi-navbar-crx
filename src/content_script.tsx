5f58c0d2a4297e78dd0dd39ed04c375cf7cb5e49
Zain Adam <zainadam.id@gmail.com>
2023-11-02 00:04:54 +0800

    chore: MUI; eslint;

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,32 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import AddressBar from './components/AddressBar'
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+  // if (msg.color) {
+  //   console.log('Receive color = ' + msg.color)
+  //   document.body.style.backgroundColor = msg.color
+  //   sendResponse('Change color to ' + msg.color)
+  // } else {
+  //   sendResponse('Color message is none.')
+  // }
+})
+
+function Navbar (): JSX.Element {
+  return (
+    <>
+      <AddressBar />
+    </>
+  )
+}
+
+const rootEl = document.createElement('div')
+rootEl.id = 'sensasi-navbar-root'
+document.body.appendChild(rootEl)
+const root = createRoot(rootEl)
+
+root.render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>
+)
--- src/options.tsx
+++ src/options.tsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
+import React, { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const Options = () => {
-  const [color, setColor] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
+const Options = (): JSX.Element => {
+  const [color, setColor] = useState<string>('')
+  const [status, setStatus] = useState<string>('')
+  const [like, setLike] = useState<boolean>(false)
 
   useEffect(() => {
     // Restores select box and checkbox state using the preferences
     // stored in chrome.storage.
     chrome.storage.sync.get(
       {
-        favoriteColor: "red",
-        likesColor: true,
+        favoriteColor: 'red',
+        likesColor: true
       },
       (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
+        setColor(items.favoriteColor)
+        setLike(items.likesColor)
       }
-    );
-  }, []);
+    )
+  }, [])
 
-  const saveOptions = () => {
+  const saveOptions = (): void => {
     // Saves options to chrome.storage.sync.
     chrome.storage.sync.set(
       {
         favoriteColor: color,
-        likesColor: like,
+        likesColor: like
       },
       () => {
         // Update status to let user know options were saved.
-        setStatus("Options saved.");
+        setStatus('Options saved.')
         const id = setTimeout(() => {
-          setStatus("");
-        }, 1000);
-        return () => clearTimeout(id);
+          setStatus('')
+        }, 1000)
+        return () => { clearTimeout(id) }
       }
-    );
-  };
+    )
+  }
 
   return (
     <>
       <div>
         Favorite color: <select
           value={color}
-          onChange={(event) => setColor(event.target.value)}
+          onChange={(event) => { setColor(event.target.value) }}
         >
           <option value="red">red</option>
           <option value="green">green</option>
@@ -57,7 +57,7 @@ const Options = () => {
           <input
             type="checkbox"
             checked={like}
-            onChange={(event) => setLike(event.target.checked)}
+            onChange={(event) => { setLike(event.target.checked) }}
           />
           I like colors.
         </label>
@@ -65,13 +65,13 @@ const Options = () => {
       <div>{status}</div>
       <button onClick={saveOptions}>Save</button>
     </>
-  );
-};
+  )
+}
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
     <Options />
   </React.StrictMode>
-);
+)
