b0fa11932d6c452694ab3013d7f1869d67db06df
Zain Adam <zainadam.id@gmail.com>
2023-11-02 22:08:04 +0800

    chore: setup navbar frame

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+
+// components
 import AddressBar from './Navbar/AddressBar'
+import NavButtons from './Navbar/NavButtons'
+import TabsBar from './Navbar/TabsBar'
 
 export default function Navbar(): React.ReactElement {
+  // TODO: Show navbar only when user hover the top of the screen (y = 0)
   return (
-    <>
-      <AddressBar />
-    </>
+    <div>
+      <TabsBar />
+
+      <div
+        style={{
+          display: 'flex',
+        }}>
+        <NavButtons />
+        <AddressBar />
+      </div>
+    </div>
   )
 }
---
+++ src/components/Navbar/NavButtons.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavButtons(): React.ReactElement {
+  return (
+    <div
+      style={{
+        border: '1px solid black',
+      }}>
+      nav buttons
+    </div>
+  )
+}
