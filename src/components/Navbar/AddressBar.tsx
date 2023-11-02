2208f5a47a2918ff76b1d43dc2444b71ecb958bb
Arya Priema <99aryaprima@gmail.com>
2023-11-02 18:34:19 +0700

    feat: Add dynamic icon change for search and explore bookmarks

--- src/components/Navbar/AddressBar.tsx
+++ src/components/Navbar/AddressBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import InputAdornment from '@mui/material/InputAdornment'
 import TextField from '@mui/material/TextField'
@@ -6,26 +6,40 @@ import TextField from '@mui/material/TextField'
 // TODO: start addortment change to search icon when user try to search (not start with http:// or https://)
 // TODO: is posible add bookmark button
 
-// import SearchIcon from '@mui/icons-material/Search'
+import SearchIcon from '@mui/icons-material/Search'
 import LanguageIcon from '@mui/icons-material/Language'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+import { IconButton, Stack } from '@mui/material'
 
 export default function AddressBar(): React.ReactElement {
+  const [value, setValue] = useState('')
+  const isSearchInput =
+    !value.startsWith('http://') && !value.startsWith('https://')
+
   return (
-    <TextField
-      size="small"
-      placeholder="Search or type a URL"
-      fullWidth
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <LanguageIcon />
-          </InputAdornment>
-        ),
-        style: {
-          colorScheme: 'inherit',
-          borderRadius: '2rem',
-        },
-      }}
-    />
+    <Stack direction="row" useFlexGap style={{ gap: '.5rem' }}>
+      <TextField
+        size="small"
+        placeholder="Search or type a URL"
+        fullWidth
+        onChange={e => {
+          setValue(e.target.value)
+        }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              {isSearchInput ? <SearchIcon /> : <LanguageIcon />}
+            </InputAdornment>
+          ),
+          style: {
+            colorScheme: 'inherit',
+            borderRadius: '2rem',
+          },
+        }}
+      />
+      <IconButton aria-label="delete">
+        <BookmarkBorderIcon />
+      </IconButton>
+    </Stack>
   )
 }
