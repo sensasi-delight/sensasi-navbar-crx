fc2726155325b95a148fef69e89734105b7f8e6f
Zain Adam <zainadam.id@gmail.com>
2023-11-03 14:16:57 +0800

    chore: add info when auto hide

--- src/components/OptionsPage/Form.tsx
+++ src/components/OptionsPage/Form.tsx
@@ -1,9 +1,11 @@
+// vendors
 import React from 'react'
 // materials
-import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import FormHelperText from '@mui/material/FormHelperText'
 import Switch from '@mui/material/Switch'
-
+// etc
 import { useAppContext } from '../../hooks/AppProvider'
 
 export default function OptionsForm(): React.ReactElement {
@@ -37,6 +39,12 @@ export default function OptionsForm(): React.ReactElement {
           control={<Switch checked={settings.isAutoHide} />}
           label="Auto Hide"
         />
+
+        {settings.isAutoHide && (
+          <FormHelperText>
+            *Move your mouse to the top of the window to show the navbar.
+          </FormHelperText>
+        )}
       </FormGroup>
     </form>
   )
