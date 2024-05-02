2d6189727c89d683496b6c72f60bb6e5ac57bb09
Zain Adam <zainadam.id@gmail.com>
2024-05-02 22:48:00 +0800

    add sentry Fixes #55

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,6 +1,7 @@
 // types
 import type { ReactElement } from 'react'
 // vendors
+import * as Sentry from '@sentry/react'
 import { createRoot } from 'react-dom/client'
 import { CacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
@@ -22,6 +23,14 @@ function Main(): ReactElement {
       <Navbar />
     </ThemeProvider>
   )
+}
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://f02bd0f29c0c6074f76bf383da2f6b2c@o1289319.ingest.us.sentry.io/4507187102613504',
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+  })
 }
 
 const rootEl = document.createElement('div')
