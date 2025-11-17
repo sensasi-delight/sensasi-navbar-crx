b9ccbb1587676bc6a527d1b2e26b5a523e3677a8
sensasi-delight <zainadam.id@gmail.com>
2025-11-17 19:21:19 +0800

    Add loading indicator component and integrate tab loading status (#85)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,16 @@ import { useEffect, useEffectEvent, useState } from 'react'
 import AddressBar from '@/components/navbar-components/address-bar'
 import NavbarAutoHideWrapper from '@/components/navbar-components/auto-hide-wrapper'
 import HotkeysDialog from '@/components/navbar-components/hotkeys-dialog'
+import LoadingIndicator from '@/components/navbar-components/loading-indicator'
 import NavButtons from '@/components/navbar-components/nav-buttons'
 import OptionsMenu from '@/components/navbar-components/options-menu'
 import TabsBar from '@/components/navbar-components/tabs-bar'
+// utils
+import sendToBgScript from '@/utils/send-to-bg-script'
 
 export default function Navbar() {
   const [isHotkeysOpen, setIsHotkeysOpen] = useState(false)
+  const [thisTabId, setThisTabId] = useState<number>()
 
   const handleKeyPress = useEffectEvent((event: KeyboardEvent): void => {
     if (event.ctrlKey && event.key === '/') {
@@ -19,6 +23,16 @@ export default function Navbar() {
       setIsHotkeysOpen(true)
     }
   })
+
+  useEffect(() => {
+    sendToBgScript(
+      'getSelfInfo',
+      undefined,
+      (response: chrome.runtime.MessageSender) => {
+        setThisTabId(response.tab?.id)
+      },
+    )
+  }, [])
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress)
@@ -30,6 +44,7 @@ export default function Navbar() {
 
   return (
     <>
+      <LoadingIndicator tabId={thisTabId} />
       <NavbarAutoHideWrapper>
         <TabsBar />
 
